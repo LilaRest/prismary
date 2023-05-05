35f8664f94875bbdd1865b14c8fe160df83d8103
Lila Rest <mail@lila.rest>
2023-05-05 16:29:10 +0200

    test: migrate from jest to vitest

--- tests/clauses.spec.ts
+++ tests/clauses.spec.ts
@@ -1,9 +1,9 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it } from 'vitest';
 import { clauses, parentClauses, extendClauses, ignoredClauses, notSupportedClauses } from "../src/clauses";
 
 describe("clauses.ts file", () => {
   const implementedClauses = new Set([...parentClauses, ...extendClauses, ...ignoredClauses, ...notSupportedClauses]);
   it("should implement all clauses", () => {
-    expect(implementedClauses).toBe(clauses);
+    expect(implementedClauses).toEqual(clauses);
   });
 });
--- tests/merge-select-or-include-clauses.spec.ts
+++ tests/merge-select-or-include-clauses.spec.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals';
+import { describe, expect, it } from 'vitest';
 import { mergeSelectOrIncludeClauses } from "../src/utils/merge-select-or-include-clauses";
 
 describe("mergeSelectOrIncludeClauses()", () => {
@@ -99,3 +99,22 @@ describe("mergeSelectOrIncludeClauses()", () => {
     // TODO
   });
 });
+
+
+/*
+ChatGPT suggestions:
+    should merge two empty select or include clauses correctly.
+    should merge two non-overlapping select clauses correctly.
+    should merge two non-overlapping include clauses correctly.
+    should merge overlapping select clauses with scalar fields correctly.
+    should merge overlapping include clauses with scalar fields correctly.
+    should merge overlapping select clauses with nested sub-bodies correctly.
+    should merge overlapping include clauses with nested sub-bodies correctly.
+    should merge a select clause with an include clause when both have non-overlapping fields correctly.
+    should merge a select clause with an include clause when both have overlapping fields correctly.
+    should merge a select clause with an include clause when both have nested sub-bodies correctly.
+    should merge complex select and include clauses with multiple levels of nested sub-bodies correctly.
+    should merge select or include clauses with a mix of scalar fields and nested sub-bodies correctly.
+    should return the correct type of merged clause (select or include) based on input clauses.
+    should preserve the original input clauses while merging.
+*/
