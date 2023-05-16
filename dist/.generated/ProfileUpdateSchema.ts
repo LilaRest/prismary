d2458558b14d9700c49d1f2d39803151662e15cf
Lila Rest <mail@lila.rest>
2023-05-16 15:06:51 +0200

    refactor(demo): move demonstration schemas and all the demo stuff inside demo/ to make things clear

--- dist/.generated/ProfileUpdateSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const ProfileUpdateSchema = z.object({
-        bio: z.string().optional(),
-        isPrivate: z.boolean().optional(),
-        age: z.number().int().optional(),
-        userId: z.number().int().optional(),
-        user: z.unknown().optional(),
-    });
